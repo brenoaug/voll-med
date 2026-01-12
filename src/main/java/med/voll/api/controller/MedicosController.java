ae8bb134b8f4c2ba622598c76ebc491dc80084ad
breno augusto <breaugustocp@outlook.com>
2026-01-12 11:17:00 -0300

    Adiciona persistência de médicos com JPA e configurações do MySQL

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
 import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.entities.Medico;
+import med.voll.api.repository.MedicoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class MedicosController {
     private static final Logger log = LoggerFactory.getLogger(MedicosController.class);
 
+    private final MedicoRepository repository;
+
+    public MedicosController(MedicoRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
     public ResponseEntity<String> cadastrar(@RequestBody DadosCadastroMedico dados){
         log.info(dados.toString());
+        repository.save(new Medico(dados));
         return ResponseEntity.ok().body("Médico cadastrado com sucesso!");
     }
 }
